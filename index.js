31cbe359ad55c75cf7c3aba6021114b9f29f530f
Thoriq Hidayansyah <toriq.mailbox@gmail.com>
2023-12-27 17:02:53 +0700

    Updating
    MEnambah podium dan tembok

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import * as THREE from './node_modules/three/build/three.module.js'
 
 import * as lib from "./lib.js"
 
+import * as GLTFLoader from './node_modules/three/examples/jsm/loaders/GLTFLoader.js'
+
+
 let camera = lib.PerspectiveCamera()
 let three = lib.threeJs(camera)
 
@@ -19,8 +22,8 @@ addEventListener('keyup', (e) => {
     keyboard[e.key] = false
 })
 
-// lighting
-lib.lighting(three.scene)
+// // lighting
+// lib.lighting(three.scene)
 
 // plane 
 var plane = new THREE.PlaneGeometry(1000, 1000, 500, 500)
@@ -72,51 +75,73 @@ three.scene.add(planeMesh)
 //     action.play()
 // })
 
+
+const geometry = new THREE.BoxGeometry( 0.5, 50, 50 ); 
+const material = new THREE.MeshPhongMaterial( {color: 0x00ff00} ); 
+const cube = new THREE.Mesh( geometry, material )
+cube.position.set(35,1,-50)
+cube.receiveShadow = true
+three.scene.add( cube );
+
+
+const geometry1 = new THREE.BoxGeometry( 0.5, 50, 50 ); 
+const material1 = new THREE.MeshPhongMaterial( {color: 0x00ff00} ); 
+const cube1 = new THREE.Mesh( geometry1, material1 )
+cube1.position.set(-30,1,-50)
+three.scene.add( cube1 );
+
+const geometry2 = new THREE.BoxGeometry( 55, 50, 1 ); 
+const material2 = new THREE.MeshPhongMaterial( {color: 0x00ff00} ); 
+const cube2 = new THREE.Mesh( geometry2, material2 )
+cube2.position.set(0,1,-75)
+three.scene.add( cube2 );
+
+
 let clockTyrano = new THREE.Clock()
 let gltfPath = 'model/tyranosaurus/scene.gltf'
-let texturePath = 'model/tyranosaurus/textures/material_0_diffuse.png'
+let texturePath = 'model/tyranosaurus/textures/material_0_specularGlossiness.png'
 let tyranoMixer
-lib.dinosaurus(three.scene, gltfPath, 2, 1, texturePath, {x: 0, y:-1, z:-10}).then(mixer => {
+lib.dinosaurus(three.scene, gltfPath, 4, 1.3, texturePath, 4.9, {x: 25, y:0, z:-30}).then(mixer => {
     tyranoMixer = mixer
 }).catch(error => {
     console.log(error)
 });
 
-let clockPtera = new THREE.Clock()
-gltfPath = 'model/pteradactal/scene.gltf'
-texturePath = 'model/pteradactal/textures/material0_baseColor.png'
-let pteraMixer
-lib.dinosaurus(three.scene, gltfPath, 0, 2, texturePath, {x:-10, y:-3, z:-10}).then(mixer => {
-    pteraMixer = mixer
-}).catch(error => {
-    console.log(error)
-});
+// let clockPtera = new THREE.Clock()
+// gltfPath = 'model/pteradactal/scene.gltf'
+// texturePath = 'model/pteradactal/textures/material0_baseColor.png'
+// let pteraMixer
+// lib.dinosaurus(three.scene, gltfPath, 0, 2, texturePath, {x:-10, y:-3, z:-10}).then(mixer => {
+//     pteraMixer = mixer
+// }).catch(error => {
+//     console.log(error)
+// });
 
-let clockStego = new THREE.Clock()
-gltfPath = 'model/stegosaurus/scene.gltf'
-texturePath = 'model/stegosaurus/textures/CH_NPC_MOB_Stego_A01_MI_HAY_normal.png'
-let stegoMixer
-lib.dinosaurus(three.scene, gltfPath, 6, 4, texturePath, {x:10, y:-1, z:-10}).then(mixer => {
-    stegoMixer = mixer
-}).catch(error => {
-    console.log(error)
-});
+// let clockStego = new THREE.Clock()
+// gltfPath = 'model/stegosaurus/scene.gltf'
+// texturePath = 'model/stegosaurus/textures/CH_NPC_MOB_Stego_A01_MI_HAY_normal.png'
+// let stegoMixer
+// lib.dinosaurus(three.scene, gltfPath, 6, 4, texturePath, {x:25, y:-1, z:-10}).then(mixer => {
+//     stegoMixer = mixer
+// }).catch(error => {
+//     console.log(error)
+// });
 
-let clockMega = new THREE.Clock()
-gltfPath = 'model/megalodon/scene.gltf'
-texturePath = 'model/megalodon/textures/material_0_clearcoat_roughness.png'
-let megaMixer
-lib.dinosaurus(three.scene, gltfPath, 0, 2, texturePath, {x:10, y:0, z:-10}).then(mixer => {
-    megaMixer = mixer
-}).catch(error => {
-    console.log(error)
-});
+// let clockMega = new THREE.Clock()
+// gltfPath = 'model/megalodon/scene.gltf'
+// texturePath = 'model/megalodon/textures/material_0_clearcoat_roughness.png'
+// let megaMixer
+// lib.dinosaurus(three.scene, gltfPath, 0, 2, texturePath, {x:10, y:0, z:-10}).then(mixer => {
+//     megaMixer = mixer
+// }).catch(error => {
+//     console.log(error)
+// });
 
 let clockVelo = new THREE.Clock()
 gltfPath = 'model/velociraptor/scene.gltf'
 texturePath = 'model/velociraptor/textures/Body_Mat_baseColor.png'
 let veloMixer
-lib.dinosaurus(three.scene, gltfPath, 3, 2, texturePath, {x:-20, y:-1, z:-10}).then(mixer => {
+lib.dinosaurus(three.scene, gltfPath, 1, 2, texturePath, 4.9, {x: 25, y:0, z:-40}).then(mixer => {
     veloMixer = mixer
 }).catch(error => {
     console.log(error)
@@ -127,26 +152,24 @@ const draw = () => {
 
     if (tyranoMixer) {
         tyranoMixer.update(clockTyrano.getDelta());
-
     }
 
-    if (pteraMixer) {
-        pteraMixer.update(clockPtera.getDelta());
+    // if (pteraMixer) {
+    //     pteraMixer.update(clockPtera.getDelta());
+    // }
+    // if (megaMixer) {
+    //     megaMixer.update(clockMega.getDelta());
 
-    }
-    if (megaMixer) {
-        megaMixer.update(clockMega.getDelta());
-
-    }
+    // }
 
     if (veloMixer) {
         veloMixer.update(clockVelo.getDelta());
 
     }
-    if (stegoMixer) {
-        stegoMixer.update(clockStego.getDelta());
+    // if (stegoMixer) {
+    //     stegoMixer.update(clockStego.getDelta());
 
-    }
+    // }
 
     three.renderer.render(three.scene, camera)
     requestAnimationFrame(draw)
--- lib.js
+++ lib.js
@@ -7,7 +7,7 @@ import * as GLTFLoader from './node_modules/three/examples/jsm/loaders/GLTFLoade
 export function PerspectiveCamera() {
     const cam = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
     cam.position.z = 7;
-    cam.position.y = 0;
+    cam.position.y = 1;
 
     return cam
 }
@@ -16,6 +16,8 @@ export function threeJs(cam) {
     const scene = new THREE.Scene();
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.BasicShadowMap;
     document.body.appendChild(renderer.domElement);
     document.body.appendChild(renderer.domElement);
     const controls = new pointerLock.PointerLockControls(cam, renderer.domElement)
@@ -44,20 +46,34 @@ export function proccesKeyboard(keyboard, controls) {
 export function lighting(scene) {
     // point light
     const pointLight = new THREE.PointLight(0xffffff, 400, 50)
-    pointLight.position.set(-4, 10, -7.5)
+    pointLight.position.set(0, 10, -30)
     pointLight.castShadow = true
     scene.add(pointLight)
     scene.add(new THREE.PointLightHelper(pointLight, 0.2, 0x00ff00))
 
-    // point light
-    const pointLight1 = new THREE.PointLight(0xffffff, 100, 50)
-    pointLight1.position.set(19, 4, -14)
-    pointLight1.castShadow = true
-    scene.add(pointLight1)
-    scene.add(new THREE.PointLightHelper(pointLight1, 0.2, 0x00ff00))
+
 }
 
-export function dinosaurus(scene, gltfPath, aniNUm, scl, texturePath, pstn) {
+export function dinosaurus(scene, gltfPath, aniNUm, scl, texturePath, r, pstn) {
+
+    // point light
+    const pointLight1 = new THREE.PointLight(0xffffff, 200, 50)
+    pointLight1.position.set(pstn.x, pstn.y + 10, pstn.z)
+    pointLight1.castShadow = true
+
+    scene.add(pointLight1)
+    scene.add(new THREE.PointLightHelper(pointLight1, 0.2, 0x00ff00))
+
+    let podium;
+    let loaderr = new GLTFLoader.GLTFLoader().load('model/podium_large/scene.gltf', function (result) {
+        podium = result.scene.children[0];
+        podium.scale.set(0.02, 0.02, 0.04)
+        podium.position.set(pstn.x, pstn.y, pstn.z)
+        podium.rotation.z = 1.6
+        podium.material = new THREE.Mesh({ receivesShadow: true });
+        scene.add(podium)
+    });
+
     return new Promise((resolve, reject) => {
         let dino;
         let animation;
@@ -81,12 +97,12 @@ export function dinosaurus(scene, gltfPath, aniNUm, scl, texturePath, pstn) {
                     if (child instanceof THREE.Mesh) {
                         child.material.map = texture;
                         child.material.side = THREE.DoubleSide;
-                        child.receiveShadow = true;
+                        // child.receiveShadow = true;
+                        child.castShadow = true
                     }
                 });
-
+                dino.rotation.z = r
                 dino.position.set(pstn.x, pstn.y, pstn.z);
-                // dino.rotation.set(0,0,0)
                 scene.add(dino);
 
                 resolve(mixer);
